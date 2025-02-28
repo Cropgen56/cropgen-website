f239245f82a846035e92a1e36ee62f4287f7d72b
vishalpatilmaske <vishpatilmaske@gmail.com>
2025-02-28 11:44:29 +0530

    create get users api

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,3 +1,17 @@
+import { NextResponse } from "next/server";
+import Subscriber from "@/app/models/Subscribe";
+import { connectToDB } from "@/app/lib/db";
+
 export async function GET() {
-  return Response.json({ message: "Hello form backend" });
+  // Connect to database
+  await connectToDB();
+  try {
+    const subscribers = await Subscriber.find();
+    return NextResponse.json({ success: true, data: subscribers });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: "Error fetching subscribers" },
+      { status: 500 }
+    );
+  }
 }
