8a47bd02ef2291ad90f735ffbda65547cc51abfb
vishalpatilmaske <vishpatilmaske@gmail.com>
2025-03-11 18:38:42 +0530

    commeted user review

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
       <AboutSection />
       <ValuesSection />
       <FeaturesSection />
-      <UserReviewsSection />
+      {/* <UserReviewsSection /> */}
       <AgricultureTechSection />
       <BlogCarousel />
       <Subscrib
