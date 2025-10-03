0ceea8452ada4dfc66df724353146bbad439c0af
vishalpatilmaske <vishpatilmaske@gmail.com>
2025-10-03 23:13:45 +0530

    add the microsoft clarity script into the website header

--- src/app/layout.js
+++ src/app/layout.js
@@ -95,6 +95,15 @@ export default function RootLayout({ children }) {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "tki6964rld");
+  `}
+        </Script>
       </head>
       <body className="antialiased">
         {/* Google Analytics */}
