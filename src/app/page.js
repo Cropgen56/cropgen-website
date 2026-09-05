825980576a821d5dcff077d5f308e5d22d3ef7b1
vishalpatilmaske <vishalpatilmaske@gmail.com>
2026-09-05 10:18:58 +0530

    update the application

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,6 @@ import ValuesSection from "@/components/home/ValueSection";
 import CollaborationBanner from "@/components/home/CollaborationBanner";
 
 export default function Home() {
-
   // const faqSchema = {
   //   "@context": "https://schema.org",
   //   "@type": "FAQPage",
@@ -117,13 +116,12 @@ export default function Home() {
       "query-input": "required name=search_term_string",
     },
   };
-  
+  // FAQ schema
 
   return (
     <div className="relative">
       <Head>
-
-          {/* <script
+        {/* <script
             type="application/ld+json"
             dangerouslySetInnerHTML={{ __html: JSON.stringify(faqSchema) }} /> */}
         <script
@@ -133,7 +131,7 @@ export default function Home() {
       </Head>
 
       <HeroSection />
-            <CollaborationBanner />
+      <CollaborationBanner />
       <AboutSection />
 
       <ValuesSection />
